79645f1e0f1a1cc30aade0b1655b88c2ddfbff75
rdk <66311944+dvuhvostka@users.noreply.github.com>
2020-12-07 15:39:10 +0300

    added pages

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var payRouter = require('./routes/pay');
+var confirmRouter = require('./routes/confirm')
+var offerRouter = require('./routes/offer')
+
 
 var app = express();
 
@@ -21,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/pay', payRouter);
+app.use('/confirm', confirmRouter);
+app.use('/offer', offerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/offer.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('offer',{
+    title: "e-giftCard - анонимность в деньгах. | Подтверждение",
+  });
+});
+
+module.exports = router;
